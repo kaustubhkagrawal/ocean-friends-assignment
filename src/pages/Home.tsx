165019609ec0fd25681c8b746814b84a6d8b41ad
Kaustubh Agrawal <kaustubh@openhouse.study>
2024-01-03 11:13:57 +0530

    Carousel Basic setup

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,11 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { CardDetails } from "./CardDetails";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 const cards = [
   {
@@ -51,37 +56,45 @@ export function Home() {
                 $2,748.00
               </p>
             </header>
-            <div
-              className={`relative max-w-full`}
-              style={{ height: width + 40 }}
-            >
-              {cards.length > 0
-                ? cards.map((card, index) => (
-                    <motion.div
-                      id={card.id}
-                      layout
-                      key={card.id}
-                      layoutId={"card" + card.id}
-                      initial={{
-                        x: (height - width) / 2 + height * index + 20 * index,
-                        opacity: 0,
-                      }}
-                      animate={{
-                        rotate: -90,
-                        y: height / 3,
-                        x: (height - width) / 2 + height * index + 20 * index,
-                        width: width,
-                        height: height,
-                        opacity: 1,
-                      }}
-                      className={`absolute top-0`}
-                      onClick={() => onCardClick(card)}
-                    >
-                      <CreditCard {...card} />
-                    </motion.div>
-                  ))
-                : null}
-            </div>
+            <Carousel>
+              <CarouselContent
+                className={`relative max-w-full -ml-5`}
+                style={{ height: width + 40 }}
+              >
+                {cards.length > 0
+                  ? cards.map((card, index) => (
+                      <CarouselItem
+                        key={card.id}
+                        style={{
+                          width: height + 20,
+                          height: width + 20,
+                          flexBasis: height + 20,
+                        }}
+                        className="pl-5"
+                      >
+                        <motion.div
+                          id={card.id}
+                          layout
+                          key={card.id}
+                          layoutId={"card" + card.id}
+                          animate={{
+                            rotate: -90,
+                            y: height / 3,
+                            x: (height - width) / 2,
+                            width: width,
+                            height: height,
+                            opacity: 1,
+                          }}
+                          className={`absolute top-0`}
+                          onClick={() => onCardClick(card)}
+                        >
+                          <CreditCard {...card} />
+                        </motion.div>
+                      </CarouselItem>
+                    ))
+                  : null}
+              </CarouselContent>
+            </Carousel>
           </>
         ) : (
           <CardDetails goBack={goBack} card={selectedCard} />
